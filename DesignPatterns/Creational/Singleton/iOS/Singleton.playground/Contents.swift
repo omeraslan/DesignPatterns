734f9481364bf8f064b3b2d611e8ec6793fe5ffa
Cemal BAYRI <cemalbayri@gmail.com>
2019-12-15 19:23:02 +0300

    added singleton design pattern

--- DesignPatterns/Creational/Singleton/iOS/Singleton.playground/Contents.swift
+++ DesignPatterns/Creational/Singleton/iOS/Singleton.playground/Contents.swift
@@ -1,3 +1,54 @@
 import UIKit
+import XCTest
 
-var str = "Hello, playground"
+protocol Writable: class {
+    func writeMessage(_ text: String)
+}
+
+final class Logger: Writable {
+    
+    //file name constructor ı
+    private var _logFileName: String = "Log.txt"
+    
+    var fileName: String! {
+        get {
+            return _logFileName
+        }
+         
+        set {
+            _logFileName = newValue
+        }
+    }
+    
+    func writeMessage(_ text: String) {
+        print("Log \(_logFileName) dosyasına yazıldı.")
+    }
+    
+    //Burası class ın static instance ını aldığımız satır. Bunu aşağıda kullanacağız. Bu satır dışında başka şekillderde de static instance alınabilir.
+    static let sharedInstance: Logger = Logger()
+}
+
+// Logger class ını burada bir kere örneğini aldık. Alınan bu instance ı projede istediğimiz yerde kullabiliriz.
+let logger = Logger.sharedInstance
+
+logger.fileName = "Test.txt"
+
+logger.writeMessage("Test mesajı")
+
+
+class LoggerTests: XCTestCase {
+
+    var logger: Logger!
+    
+    func testLoggerInstance() {
+        logger = Logger.sharedInstance
+        XCTAssertTrue(logger != nil, "Logger singleton class ı oluşturuldu.")
+    }
+
+}
+
+LoggerTests.defaultTestSuite.run()
+
+
+
+
